6350b72225ec7408028d1f9b60bc41bd877aca60
ggp <937827303@qq.com>
2021-10-18 23:09:31 +0800

    注册中心-eureka、consul、nocos

--- noob-cloud-business-demo/client-consumer/src/main/java/com/noob/cloud/business/demo/client/consumer/ConsumerApplication.java
+++ noob-cloud-business-demo/client-consumer/src/main/java/com/noob/cloud/business/demo/client/consumer/ConsumerApplication.java
@@ -2,12 +2,14 @@ package com.noob.cloud.business.demo.client.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author:ggp
  * @Date:2021/10/13 20:01
  * @Description:
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
